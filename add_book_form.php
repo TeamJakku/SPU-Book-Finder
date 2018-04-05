6597cb4e50137c915ac81873a4292a1e63a66e09
Monica Guevara <32884899+monicagi@users.noreply.github.com>
2018-04-05 11:39:43 -0700

    Add files via upload

---
+++ add_book_form.php
@@ -0,0 +1,120 @@
+<html>  
+
+<html lang="en">  
+
+<head>  
+  <title>Add Book</title>
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</head>  
+
+<body>
+<h1>Welcome to SPU book finder</h1>
+<div class="topnav">
+  <a class="active" href="edit-account.html">My Account</a>
+  <a class="active" href="index.php">Search Book</a>
+  <a href="add_book_form.php">Add Book</a>
+   <a href="edit_posts.html">Edit</a>
+
+</div> 
+<form class="form-horizontal" action="add_joke.php">
+<fieldset>
+
+<!-- Form Name -->
+<legend>Add Book</legend>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="isbn">ISBN:</label>  
+  <div class="col-md-4">
+  <input name="isbn" class="form-control input-md" id="isbn" type="text" placeholder="9999999999">
+  <span class="help-block">Enter the ISBN of book</span>  
+  </div>
+</div>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="Title">Title</label>  
+  <div class="col-md-4">
+  <input name="Title" class="form-control input-md" id="Title" type="text" placeholder="e.g Algorithm Design ">
+  <span class="help-block">Enter title of the book</span>  
+  </div>
+</div>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="author">Author</label>  
+  <div class="col-md-4">
+  <input name="author" class="form-control input-md" id="author" type="text" placeholder="Jon Kleinberg, Eva Tardos">
+  <span class="help-block">Enter author</span>  
+  </div>
+</div>
+
+<!-- Text input-->
+<div class="form-group" >
+  <label class="col-md-4 control-label" for="price">Price</label>  
+  <div class="col-md-4">
+  <input name="price" class="form-control input-md" id="price" type="text" placeholder="70.00">
+  <span class="help-block">Enter price of book (no dollar sign)</span>  
+  </div>
+</div>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="course">Course</label>  
+  <div class="col-md-4">
+  <input name="course" class="form-control input-md" id="course" type="text" placeholder="CSC 3430">
+  <span class="help-block">Enter the course for this book</span>  
+  </div>
+</div>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="edition">Edition</label>  
+  <div class="col-md-4">
+  <input name="edition" class="form-control input-md" id="edition" type="text" placeholder="7th (optional)">
+    
+  </div>
+</div>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="condition">Condition</label>  
+  <div class="col-md-4">
+  <input name="condition" class="form-control input-md" id="condition" type="text" placeholder="used/new">
+    
+  </div>
+</div>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="description">Description</label>  
+  <div class="col-md-4">
+  <input name="description" class="form-control input-md" id="description" type="text" placeholder="(optional)">
+    
+  </div>
+</div>
+
+<!-- Button -->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="singlebutton"></label>
+  <div class="col-md-4">
+    <button name="add_book_button" class="btn btn-primary" id="singlebutton">SUBMIT</button>
+  </div>
+</div>
+
+</fieldset>
+</form>
+  
+
+</body>  
+
+</html>
---
+++ index.php
@@ -0,0 +1,124 @@
+<html>
+<head>
+  <title>Welcome to SPU Book Finder</title>
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+<h1>Welcome to SPU book finder</h1>
+<div class="topnav">
+	<a class="active" href="edit-account.html">My Account</a>
+  <a class="active" href="index.php">Search Book</a>
+  <a href="add_book_form.php">Add Book</a>
+   <a href="edit_posts.html">Edit</a>
+
+</div> 
+<?php
+
+include "db_connection.php";
+
+//include "search_all_jokes.php";
+
+?>
+
+
+<!-- search books with key workd -->
+<form class="form-horizontal" action="search_keyword.php">
+<fieldset>
+
+<!-- Form Name -->
+<legend>Search</legend>
+
+<!-- Search input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="keyword">Search for books</label>
+  <div class="col-md-5">
+    <input name="keyword" class="form-control input-md" id="keyword" required="" type="search" placeholder="e.g. data structures">
+    <p class="help-block">Enter a word to search in book database</p>
+  </div>
+</div>
+
+<!-- Button -->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="Search Button"></label>
+  <div class="col-md-4">
+    <button name="Search Button" class="btn btn-primary" id="Search Button">Search</button>
+  </div>
+</div>
+
+</fieldset>
+</form>
+<!-- search books by selected course  -->
+<form class="form-horizontal" action="save_by_course.php">
+<fieldset>
+
+<!-- Form Name -->
+<legend>Search by course</legend>
+
+<!-- Select Basic -->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="selectbasic">Select Course</label>
+  <div class="col-md-4">
+    <select name="selectbasic" class="form-control" id="selectbasic">
+      <option value="CSC 143">Data Structures Java</option>
+      <option value="CSC 3430">Algorithm Design</option>
+      <option value="CSC 2430">Data Structures C++</option>
+      <option value="CSC 3150">System Design </option> 
+    </select>
+  </div>
+</div>
+
+<div class="form-group">
+  <label class="col-md-4 control-label" for="Search Button"></label>
+  <div class="col-md-4">
+    <button name="Search Button" class="btn btn-primary" id="Search Button">Search</button>
+  </div>
+</div>
+
+</fieldset>
+</form>
+<!-- search all the books in the database-->
+<form class="form-horizontal" action ="search_all_jokes.php">
+<fieldset>
+
+<!-- Form Name -->
+<legend>Search all books</legend>
+
+<!-- Button -->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="singlebutton">Search all books in database</label>
+  <div class="col-md-4">
+    <button name="singlebutton" class="btn btn-primary" id="singlebutton">Search</button>
+  </div>
+</div>
+
+</fieldset>
+</form>
+
+<form action="add_joke.php">
+	
+
+<?php
+//search jokes with key word
+
+//include "search_keyword.php";
+
+$mysqli->close();
+
+
+?>
+</body>
+
+
+
+
+</html>
