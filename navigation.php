2d848936aefcd0ca09a5690da038330270f7b9d3
Celena F <32528673+celenaf@users.noreply.github.com>
2018-04-06 08:23:32 -0700

    Update navigation.php

--- navigation.php
+++ navigation.php
@@ -1,6 +1,6 @@
 <div class="topnav">
-			<a href="edit-account.html">My Account</a></li>
-			<a href="search.html">Search Book</a></li>
-			<a href="add_book_form.html">Add Book</a></li>
-			<a href="edit_posts.html">Edit</a></li>
+	<a href="edit-account.html">My Account</a>
+	<a href="search.html">Search Book</a>
+	<a href="add_book_form.html">Add Book</a>
+	<a href="edit_posts.html">Edit</a>
 </div>
