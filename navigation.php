e91a50bd3014de1e57aa36f4938960cb48d593a7
Celena F <32528673+celenaf@users.noreply.github.com>
2018-04-06 08:24:12 -0700

    Delete navigation.php

--- navigation.php
+++
@@ -1,6 +0,0 @@
-<div class="topnav">
-	<a href="edit-account.html">My Account</a>
-	<a href="search.html">Search Book</a>
-	<a href="add_book_form.html">Add Book</a>
-	<a href="edit_posts.html">Edit</a>
-</div>
