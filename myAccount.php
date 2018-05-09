f7fc4b7d0d15362f58dc5b9f51ace66cf828cf80
Monica Guevara <32884899+monicagi@users.noreply.github.com>
2018-05-08 23:42:03 -0700

    Add files via upload

--- myAccount.php
+++ myAccount.php
@@ -30,7 +30,7 @@
 </div>
 
 
-<span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776;</span>
+<span style="font-size:30px; cursor:pointer; color:#FFF2CC;" onclick="openNav()">&#9776;</span>
 
 <script>
 function openNav() {
