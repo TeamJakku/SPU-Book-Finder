e9e5bb184bdbb794496b3f43cdcfed22357ef4af
Monica Guevara <32884899+monicagi@users.noreply.github.com>
2018-05-09 00:38:01 -0700

    Add files via upload

--- delete_post.php
+++ delete_post.php
@@ -1,55 +1,7 @@
 <head>
 <meta name="viewport" content= "width-device-width, initial-scale=1">
+<link rel="stylesheet" href="style.css">
 <style>
-
-	body {
-    font-family: "Lato", sans-serif;
-}
-
-.sidenav {
-    height: 100%;
-    width: 0;
-    position: fixed;
-    z-index: 1;
-    top: 0;
-    left: 0;
-    background-color: #3366cc;
-    overflow-x: hidden;
-    transition: 0.5s;
-    padding-top: 60px;
-}
-
-.sidenav a {
-    padding: 8px 8px 8px 32px;
-    text-decoration: none;
-    font-size: 25px;
-    color: #ffffff;
-    display: block;
-    transition: 0.3s;
-}
-
-.sidenav a:hover {
-    color: #ffcc99;
-}
-
-.sidenav .closebtn {
-    position: absolute;
-    top: 0;
-    right: 25px;
-    font-size: 36px;
-    margin-left: 50px;
-}
-#main {
-    transition: margin-left .5s;
-    padding: 20px;
-}
-
-@media screen and (max-height: 450px) {
-  .sidenav {padding-top: 15px;}
-  .sidenav a {font-size: 18px;}
-}
-
-
 </style> 
   
   
@@ -64,7 +16,7 @@
 </head>
 
 <?php
-session_start();
+//session_start();
 include "db_connection.php";
 
 echo"<h2>All books in database</h2>";
@@ -83,14 +35,13 @@ $result = $mysqli->query($sql);
 <h1>SPU Book Finder</h1>
 <div id="mySidenav" class="sidenav">
   <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-  <a href="index.php">Search</a>
+  <a href="index_search.php">Search</a>
   <a href="add_book_form.php">Post</a>
   <a href="delete_post.php">Delete</a>
-    <a href="email_chat.php">Message</a>
+  <a href="email_chat.php">Message</a>
   <a href="myAccount.php">My Account</a>
   <a href="logout.php">Log Out</a>
 </div>
-
 
 <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; Menu</span>
 
