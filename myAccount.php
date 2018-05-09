a053d302be38b260d096becab786169ae4ffb00a
Monica Guevara <32884899+monicagi@users.noreply.github.com>
2018-05-08 21:14:29 -0700

    Working on retrieving username and email

--- myAccount.php
+++ myAccount.php
@@ -1,60 +1,13 @@
+
 <html>  
 
 <html lang="en">  
 
 <head>
+<title>My Account</title>
 <meta name="viewport" content= "width-device-width, initial-scale=1">
-<style>
+<link rel="stylesheet" href="style.css">
 
-	body {
-    font-family: "Lato", sans-serif;
-}
-
-.sidenav {
-    height: 100%;
-    width: 0;
-    position: fixed;
-    z-index: 1;
-    top: 0;
-    left: 0;
-    background-color: #3366cc;
-    overflow-x: hidden;
-    transition: 0.5s;
-    padding-top: 60px;
-}
-
-.sidenav a {
-    padding: 8px 8px 8px 32px;
-    text-decoration: none;
-    font-size: 25px;
-    color: #ffffff;
-    display: block;
-    transition: 0.3s;
-}
-
-.sidenav a:hover {
-    color: #ffcc99;
-}
-
-.sidenav .closebtn {
-    position: absolute;
-    top: 0;
-    right: 25px;
-    font-size: 36px;
-    margin-left: 50px;
-}
-#main {
-    transition: margin-left .5s;
-    padding: 20px;
-}
-
-@media screen and (max-height: 450px) {
-  .sidenav {padding-top: 15px;}
-  .sidenav a {font-size: 18px;}
-}
-
-
-</style>  
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
@@ -64,10 +17,11 @@
 <!-- Latest compiled JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
-<h1>SPU Book Finder</h1>
+<div class="ix">
+<h1 style = "background-color: #7F1335;" class ="log">SPU Book Finder</h1>
 <div id="mySidenav" class="sidenav">
   <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-  <a href="index.php">Search</a>
+  <a href="index_search.php">Search</a>
   <a href="add_book_form.php">Post</a>
   <a href="delete_post.php">Delete</a>
   <a href="email_chat.php">Message</a>
@@ -76,7 +30,7 @@
 </div>
 
 
-<span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; Menu</span>
+<span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776;</span>
 
 <script>
 function openNav() {
@@ -91,12 +45,60 @@ function closeNav() {
 } 
 </script>
 
+<?php
+include_once ("db_connection.php");
+
+//$username = strip_tags($_POST['username']);
+//$username = stripslashes($username);
+//$username = mysqli_real_escape_string($mysqli, $username);
+$username = mysqli_real_escape_string($mysqli, $_GET['username']);
+echo $_SESSION['username'];
+
+//$email = strip_tags($_POST['email']);
+//$email = stripslashes($email);
+$email = mysqli_real_escape_string($mysqli, $_GET['email']);
+
+
+if($mysqli->connect_errno){
+	echo "Failed to connect to MySQL; (". $mysqli->connect_errno.")".$mysqli->connect_errno;
+}
+// echo $mysqli->host_info. "\n";
+
+$sql = "SELECT username, email FROM users WHERE username = '$username'";
+//$result = $mysqli->query($sql);
+$result = mysqli_query($mysqli, $sql);
+
+if (!$result && mysqli_num_rows($result) != 1)
+    {
+      echo "Error: Data not found..";
+    }
+    $test = mysqli_fetch_array($result);
+    $username = $test['username'];
+	echo $username;
+?>
+
+<?php
+
+if($result->num_rows>0){
+	//output data of each row
+	while($row = $result->fetch_assoc()){
+		
+		echo "<h3>$row[title]</h3>";
+	  echo "<div><p>Username: $row[username]</p><p>Email: $row[email]</p>";
+	}
+}else{
+	echo "0 results";
+}
+?>
+
 <body>
 
 </div>
 
-<legend>My Account</legend>
+<legend style = "background-color: #7F1335; color: #FFF2CC;">My Account</legend>
 <!-- Code for Sign Up Fields-->
+<div class="sechalf">
+<div id="main">
     <div class="form-group">
         <label class="col-md-4 control-label" for="register"></label>
         <div class="col-md-5">
@@ -136,7 +138,7 @@ function closeNav() {
     <div class="form-group">
         <label class="col-md-4 control-label" for="Search Button"></label>
         <div class="col-md-4">
-        <button name="register" class="btn btn-primary" id="Register button">Save</button>
+        <button style = "background-color: #7F1335;" name="register" class="btn btn-primary" id="Register button">Save</button>
         </div>
     </div>
     
@@ -147,9 +149,10 @@ function closeNav() {
         </div>
     </div>
 -->
+    </div>
     </fieldset>
     </form>
-  
+</div>  
 
 </body>  
 
